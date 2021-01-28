bb7de9082f9f226da9d3533d5e8d9e93f625c35e
godotto <m.marcinkiewicz@protonmail.com>
2021-01-28 18:47:19 +0100

    Add C main function It will call firstconst function created in x86 assembly language

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned int firstconst(char *str); // function body is implemented in firstconst.s
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Program requires a string as an argument\n");
+        return -1;
+    }
+    
+    char *input = strdup(argv[1]);
+    printf("First integer in string: %d\n", firstconst(input));
+    free(input);
+
+    return 0;
+}
